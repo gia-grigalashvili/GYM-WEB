b84464ad4cd113543abfba3c845a637e1b562380
gia-grigalashvili <grigalagia07@gmail.com>
2025-01-11 13:47:42 +0400

    ,ak email input

--- src/components/Contact/Contact.jsx
+++ src/components/Contact/Contact.jsx
@@ -72,6 +72,20 @@ export default function Contact() {
             <p className="text-red-500 text-sm mt-2">{errors.user_name}</p>
           )}
         </div>
+        <div>
+          <input
+            type="text"
+            name="user_email"
+            className={`mt-1 px-3 py-2 bg-transparent text-white placeholder:text-[#C4C4C499] border rounded-lg w-full focus:outline-none focus:ring-2 ${
+              errors.user_email ? "border-red-500" : "border-[#4D4D4D]"
+            } focus:ring-blue-500 focus:border-blue-500`}
+            placeholder="Email"
+            required
+          />
+          {errors.user_email && (
+            <p className="text-red-500 text-sm mt-2">{errors.user_email}</p>
+          )}
+        </div>
       </form>
     </div>
   );

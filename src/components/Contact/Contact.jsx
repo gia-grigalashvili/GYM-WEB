7cb9aee925da7b16bad07b3b176da8078cea1492
gia-grigalashvili <grigalagia07@gmail.com>
2025-01-11 13:48:07 +0400

    make sumbit input

--- src/components/Contact/Contact.jsx
+++ src/components/Contact/Contact.jsx
@@ -86,6 +86,43 @@ export default function Contact() {
             <p className="text-red-500 text-sm mt-2">{errors.user_email}</p>
           )}
         </div>
+        <div>
+          <input
+            type="number"
+            name="user_phone"
+            className={`mt-1 px-3 py-2 bg-transparent text-white placeholder:text-[#C4C4C499] border rounded-lg w-full focus:outline-none focus:ring-2 ${
+              errors.user_phone ? "border-red-500" : "border-[#4D4D4D]"
+            } focus:ring-blue-500 focus:border-blue-500 no-arrows`}
+            placeholder="Phone number"
+            required
+          />
+          {errors.user_phone && (
+            <p className="text-red-500 text-sm mt-2">{errors.user_phone}</p>
+          )}
+        </div>
+        <div>
+          <textarea
+            name="message"
+            className={`mt-1 px-3 py-2 bg-transparent text-white placeholder:text-[#C4C4C499] border rounded-lg w-full focus:outline-none focus:ring-2 ${
+              errors.message ? "border-red-500" : "border-[#4D4D4D]"
+            } focus:ring-blue-500 focus:border-blue-500`}
+            rows="5"
+            placeholder="Write your message"
+            required
+          />
+          {errors.message && (
+            <p className="text-red-500 text-sm">{errors.message}</p>
+          )}
+        </div>
+
+        <div className="flex justify-end">
+          <input
+            type="submit"
+            value="Send"
+            className="px-4 py-2 border-[#4D4D4D] border-[1px] text-[#C4C4C499] w-[35%] rounded-lg  hover:text-black  duration-700 hover:bg-[#D7FD44] hover:scale-105 hover:transition-transform cursor-pointer"
+          />
+          <ToastContainer />
+        </div>
       </form>
     </div>
   );

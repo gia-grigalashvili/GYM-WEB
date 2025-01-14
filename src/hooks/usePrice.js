c4bb4959d3978bd5b1d4c162e840cf114f48d944
gia-grigalashvili <grigalagia07@gmail.com>
2025-01-15 00:15:33 +0400

    make isloading and iserror

--- src/hooks/usePrice.js
+++ src/hooks/usePrice.js
@@ -1,5 +1,5 @@
 import { useQuery } from "@tanstack/react-query";
-import { fetchPrices } from "../services/GymApi";
+import { fetchPrices } from "../services/FetchPrices";
 
 export const usePrice = () => {
   return useQuery({
--- src/pages/Servicies.jsx
+++ src/pages/Servicies.jsx
@@ -1,12 +1,16 @@
 import React from "react";
 import Navigation from "../components/Header/Nanvigation";
 import ServicesSlider from "../components/SliderSerivce/SerivcesSlider";
+import Prices from "../components/Prices/Prices";
+import Footer from "../components/Fotter/Footer";
 
 export default function Servicies() {
   return (
     <div>
       <Navigation />
       <ServicesSlider />
+      <Prices />
+      <Footer />
     </div>
   );
 }

cc13bf6985b9e90c42679c6fa0aa401aadec0a43
gia-grigalashvili <grigalagia07@gmail.com>
2025-01-15 23:08:27 +0400

    add blogage in app

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,7 @@ import Stories from "./pages/Stories";
 import HomePage from "./pages/HomePage";
 import Servicies from "./pages/Servicies";
 import Contact from "./pages/Contact";
+import BlogsPage from "./pages/BlogsPage";
 function App() {
   return (
     <>
@@ -12,6 +13,7 @@ function App() {
           <Route path="/Stories" element={<Stories />} />
           <Route path="/Services" element={<Servicies />} />
           <Route path="/Contact" element={<Contact />} />
+          <Route path="/Blogs" element={<BlogsPage />} />
         </Route>
       </Routes>
     </>

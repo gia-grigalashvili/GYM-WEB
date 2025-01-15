992bdcea4d7a6ee35912e2241a8809da6e441473
gia-grigalashvili <grigalagia07@gmail.com>
2025-01-15 23:06:39 +0400

    start Blogspage

---
+++ src/pages/BlogsPage.jsx
@@ -0,0 +1,9 @@
+import React from "react";
+import Navigation from "../components/Header/Nanvigation";
+export default function BlogsPage() {
+  return (
+    <div>
+      <Navigation />
+    </div>
+  );
+}

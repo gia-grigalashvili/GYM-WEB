a6cc11a2248fd0a51d09e1a81629e075c222a24c
gia-grigalashvili <grigalagia07@gmail.com>
2025-01-21 01:20:29 +0400

    make sharestory

--- src/components/AdminPanel/AdminPanel/AdminAbout/AboutMain.jsx
+++ src/components/AdminPanel/AdminPanel/AdminAbout/AboutMain.jsx
@@ -1,10 +1,12 @@
 import React from "react";
 import AboutHeader from "./AboutHeader";
+import Sharestory from "./Sharestory";
 
 export default function AboutMain() {
   return (
-    <div>
+    <div className="flex flex-col gap-[50px]">
       <AboutHeader />
+      <Sharestory />
     </div>
   );
 }
---
+++ src/components/AdminPanel/AdminPanel/AdminAbout/Sharestory.jsx
@@ -0,0 +1,33 @@
+import React from "react";
+import { useFetchAbout } from "../../../../hooks/useFetchAbout";
+export default function Sharestory() {
+  const { data, isLoading, error, isError } = useFetchAbout();
+  console.log(data);
+  const { story, experience } = data.about[0];
+  return (
+    <div className="flex flex-col gap-[50px]">
+      {" "}
+      <div className="flex flex-col w-full gap-3">
+        <p className="text-white">Share your story</p>
+        <textarea
+          className="placeholder:w-[34rem] w-full p-[0.625rem] rounded-2xl bg-[#323232] text-white font-light placeholder:text-[#C4C4C4]"
+          placeholder="Hi, I'm Tuna, a personal trainer dedicated to helping people transform through fitness. My journey began when I overcame my own struggles with body confidence and health. Now, I use my experience to empower others to achieve their fitness goals..."
+          name="story"
+          cols="30"
+          rows="7"
+          defaultValue={story}
+        ></textarea>
+      </div>
+      <div>
+        <p className="text-white">Experience</p>
+        <input
+          type="number"
+          name="experience"
+          placeholder="add your experience"
+          className="placeholder:w-[34rem] w-full p-[0.625rem] rounded-2xl bg-[#323232] text-white font-light placeholder:text-[#C4C4C4]"
+          defaultValue={experience}
+        />
+      </div>
+    </div>
+  );
+}

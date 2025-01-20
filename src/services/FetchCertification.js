e1682da2ac552b06138037546e8d47164abd9e41
gia-grigalashvili <grigalagia07@gmail.com>
2025-01-21 01:29:17 +0400

    add fetchCertification

---
+++ src/services/FetchCertification.js
@@ -0,0 +1,6 @@
+import { supabase } from "../configs/supabase";
+
+export const fetchCertification = async () => {
+  let { data, error } = await supabase.from("certification").select("*");
+  return { data, error };
+};
